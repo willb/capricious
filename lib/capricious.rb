8488d31c26005d364815d2a0ee75134cee573e40
Will Benton <willb@redhat.com>
2010-03-11 12:24:45 -0600

    capricious.rb now loads all necessary libs

--- lib/capricious.rb
+++ lib/capricious.rb
@@ -16,6 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-module Capricious
-  
-end
+require 'capricious/lfsr'
+require 'capricious/sample_sink'
+require 'capricious/uniform'
